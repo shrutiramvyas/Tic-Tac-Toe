9f402bfaa0056ef40a7546009f87d0418cabb1c9
Shruti <sdnk1993@gmail.com>
2022-11-18 03:03:40 +0530

    second Use case

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -12,12 +12,30 @@ public class TicTacToeGame {
     public TicTacToeGame(String[] board) {
         TicTacToeGame.board = board;
     }
+    public static String checkLetter(String check){
+        switch(check){
+            case "X":
+            case "x":
+                return "X";
+            case "o":
+            case "O":
+                return "O";
+            default:
+                System.out.println("Wrong choice");
+                Scanner scan = new Scanner(System.in);
+                check = scan.nextLine();
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please enter your name: ");
         String userName = scan.nextLine();
         System.out.println("Welcome "+ userName +" to the World of Tic-Tac-Toe Game");
+        System.out.println(userName + ", Please choose your Favourite word 'X' or 'O' ");
+        String check = scan.nextLine();
+        String cLetter = checkLetter(check);
         String[] board = new String[10];
         for(int i = 0; i < 10; i++){
             board[i]=String.valueOf(i+1);
